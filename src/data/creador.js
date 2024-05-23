5fbd63d16cb66d47b9c7033f9dfe40df53db14f8
20220270 <136751152+20220270@users.noreply.github.com>
2024-05-22 21:16:11 -0600

    ejercicio de select

---
+++ src/data/creador.js
@@ -0,0 +1,9 @@
+const creadorData = [
+{
+        id: '1',
+        name: 'Ivansito Salguero',
+        desc: `Ejercicio 3, se modificó para mostrar colores y más datos`
+}
+];
+
+export default creadorData;
---
+++ src/data/data.js
@@ -0,0 +1,24 @@
+const Data = [
+    {
+      id: '1',
+      title: 'Pupusita',
+      src: require('../img/pupusas-comida.jpg'),
+    },
+    {
+      id: '2',
+      title: 'Atolito de Elote',
+      src: require('../img/atol-bebida.jpg'),
+    },
+    {
+      id: '3',
+      title: 'Sopón de pata',
+      src: require('../img/sopa-de-pata.jpg'),
+    },
+    {
+      id: '4',
+      title: 'Tamales de Elote',
+      src: require('../img/tamales-comida.jpg'),
+    }
+  ];
+
+  export default Data;
